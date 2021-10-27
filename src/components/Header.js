476bfe0578a71c4edcf50f990ba4dde468ac8da7
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-10-27 14:19:03 +0100

    added onclick

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import Button from "./Button";
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log("click")
+    }
+
+
     return (
         <div>
             <h1>
                 {title}
             </h1>
+
+            <Button color='green' text='Add' onClick={onClick}/>
+
         </div>
     );
 };
