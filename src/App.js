65444a416f31fe1e38c08bbddd776eeba77331d6
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-10-27 14:32:28 +0100

    added tasks component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     return (
         <div className='container'>
             <Header />
+            <Tasks />
         </div>
     );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,36 @@
+import {useState} from 'react'
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([
+
+        {
+            id: 1,
+            text: 'Doctors appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Meeting at school',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        }
+    ])
+    return (
+        <>
+            {tasks.map((task) => (
+                <h3 key={task.id}>
+                    {task.text}
+                </h3>
+            ))}
+        </>
+    );
+};
+
+export default Tasks;
