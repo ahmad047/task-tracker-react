5999d8184a123cb88fb52e11200a193f1aafb9bd
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-10-27 13:40:27 +0100

    basic styling added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const Header = () => {
+const Header = ({title}) => {
     return (
         <div>
-            <h1>Task Tracker</h1>
+            <h1>
+                {title}
+            </h1>
         </div>
     );
 };
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.protoTypes = {
+    title: PropTypes.string.isRequired
+}
+
+// css in js
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
+
 
 export default Header;
