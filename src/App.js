32c7b7cb1d1567b587458131f7488cfa92fe9ac5
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-10-27 16:57:07 +0100

    can now get all tasks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import AddTask from "./components/AddTask";
 
 
 const App = () => {
     const [showAddTask, setShowAddTask] = useState(false)
 
-    const [tasks, setTasks] = useState([
+    const [tasks, setTasks] = useState([])
 
-        {
-            id: 1,
-            text: 'Doctors appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true
-        },
-        {
-            id: 2,
-            text: 'Meeting at school',
-            day: 'Feb 6th at 2:30pm',
-            reminder: true
-        },
-        {
-            id: 3,
-            text: 'shopping',
-            day: 'Feb 7th at 2:30pm',
-            reminder: true
+    useEffect(() => {
+        const getTasks = async () => {
+            const tasksFromServer = await fetchTasks()
+            setTasks(tasksFromServer)
         }
-    ])
+
+        getTasks()
+    }, [])
+
+    const fetchTasks = async() => {
+        const res = await fetch('http://localhost:5000/tasks')
+        const data = await res.json()
+
+        return data
+    }
+
 
     const deleteTask = (id) => {
+
+
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
