cd7ff1a58fc837bdcec4253b3d55763aed60071a
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-10-27 14:43:16 +0100

    added font awesome

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import {useState} from 'react'
 
 
 const App = () => {
 
+    const [tasks, setTasks] = useState([
+
+        {
+            id: 1,
+            text: 'Doctors appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Meeting at school',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        }
+    ])
+
     return (
         <div className='container'>
             <Header />
-            <Tasks />
+            <Tasks tasks={tasks}/>
         </div>
     );
 }
