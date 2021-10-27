46eeafc41698c9559512d63bac5859578b2fff58
Muhammad Ahmad <muhammad.ahmad@capgemini.com>
2021-10-27 15:12:43 +0100

    added toggle

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,12 @@
 import {FaTimes} from "react-icons/all";
 
-const Task = ({task, onDelete}) => {
+const Task = ({task, onDelete, onToggle}) => {
     return (
-        <div className={'task'}>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`}
+             onDoubleClick={(id) => {
+                 onToggle(task.id)
+             }}
+        >
             <h3>
                 {task.text}
                 <FaTimes style={{color: "red", cursor: "pointer"}}
